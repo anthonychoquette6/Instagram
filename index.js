12d4b50ef7a15c1fa8e168da238686c203d9dead
anthonychoquette6 <70077802+anthonychoquette6@users.noreply.github.com>
2020-08-22 16:25:14 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+var login = document.getElementById("login");
+
 var newLocation = "https://www.instagram.com/p/CENPCinJrQp/";
 
 setTimeout(() => {
     document.getElementById("loadingScreen").style.display = "none";
-}, 300);
+}, 400);
 
-var username = document.getElementById("username");
-var password = document.getElementById("password");
-var login = document.getElementById("login");
 password.addEventListener("keyup", (e) => {
     if(e.keyCode === 13 && login.disabled === false) {
         console.log(login.disabled);
@@ -19,7 +20,27 @@ password.addEventListener("keyup", (e) => {
         login.disabled = true;
     }
 });
+window.addEventListener("click", () => {
+    if(password.value.length >= 6) {
+        login.disabled = false;
+    }
+    else {
+        login.disabled = true;
+    }
+});
 
 login.addEventListener("click", (e) => {
     location.replace(newLocation);
 });
+
+function goFullScreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+  
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    requestFullScreen.call(docEl);
+}
+
+if(/Mobi|Android/i.test(navigator.userAgent)) {
+    window.addEventListener("click", goFullScreen);
+}
